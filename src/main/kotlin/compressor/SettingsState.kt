c958000508778ccb62011db1fba0e25426ee0c96
kokokochet <kokokochetov@gmail.com>
2023-06-05 02:18:02 +0400

    Человеческие пояснения Обновил readme и добавил пояснения при наведении на настройки

--- src/main/kotlin/compressor/SettingsState.kt
+++ src/main/kotlin/compressor/SettingsState.kt
@@ -6,6 +6,7 @@ class SettingsState {
         val values: List<String>,
         val toStringCurrentValue: (T) -> String = { it.toString() },
         val fromStringCurrentValue: (T) -> T = { it },
+        val explanations: String = "",
         val toT: (String) -> T
     ) {
         fun setCurrentValue(value: String) {
@@ -14,12 +15,21 @@ class SettingsState {
         fun currentValueString() = toStringCurrentValue(currentValue)
     }
 
-    val videoCodec = SettingProperty("libx264", listOf("libx264", "h264_nvenc", "h264_amf"), toT = { it })
+    val videoCodec = SettingProperty(
+        "libx264", listOf("libx264", "h264_nvenc", "h264_amf", "h264_qsv"),
+        explanations = "позволяет включить аппаратное ускорение для обработки видео на видеокарте\n" +
+                "h264_nvenc - для nvidia\n" +
+                "h264_amf - для AMD\n" +
+                "h264_qsv - для Intel\n" +
+                "libx264 - если не уверены, будет работать на процессоре",
+        toT = { it }
+    )
     val videoBitrate = SettingProperty(
         300 * 1024,
         (20..900 step 80).map { it.toString() }.toList(),
         { (it / 1024).toString() },
         { it * 1024 },
+        "задает максимальный битрейт видео в килобитах",
         { it.toInt() }
     )
     val audioBitrate = SettingProperty(
@@ -27,16 +37,19 @@ class SettingsState {
         (20..320 step 20).map { it.toString() }.toList(),
         { (it / 1000).toString() },
         { it * 1000 },
+        "задает максимальный битрейт аудио в килобитах, так же используется для звука в видео",
         { it.toInt() }
     )
     val cpuCores = SettingProperty(
         Runtime.getRuntime().availableProcessors(),
         (1..Runtime.getRuntime().availableProcessors()).map { it.toString() }.toList(),
+        explanations = "задает число ядер, которое будет использовать FFmpeg",
         toT = { it.toInt() }
     )
     val imageResolution = SettingProperty(
         1000,
         (300..2000 step 100).map { it.toString() }.toList(),
+        explanations = "задает максимальное разрешение изображения в пикселях",
         toT = { it.toInt() }
     )
 
--- src/main/kotlin/ui/SettingsUI.kt
+++ src/main/kotlin/ui/SettingsUI.kt
@@ -5,29 +5,61 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.unit.dp
 import compressor.SettingsState
 
+data class Explanation(
+    val actual: String? = null,
+    val default: String = "тут будут пояснения если навести на настроки"
+)
+
 @Composable
 fun settingsView(set: SettingsState, scope: BoxScope, onReady: () -> Unit) = scope.apply {
+    var explanations by remember {
+        mutableStateOf(Explanation())
+    }
+
     Column {
         textButton("ГОТОВО", onClick = onReady)
-        settingsRow("FFmpeg set", set.videoCodec)
-        settingsRow("max video bitrate kbit/s", set.videoBitrate)
-        settingsRow("max audio bitrate kbit/s", set.audioBitrate)
-        settingsRow("max cpu cores use", set.cpuCores)
-        settingsRow("maximum image resolution", set.imageResolution)
+        Column {
+            settingsRow("FFmpeg encoder", set.videoCodec) {
+                explanations = Explanation(it)
+            }
+            settingsRow("макс битрейт видео", set.videoBitrate) {
+                explanations = Explanation(it)
+            }
+            settingsRow("макс битрейт аудио", set.audioBitrate) {
+                explanations = Explanation(it)
+            }
+            settingsRow("число ядер под FFmpeg", set.cpuCores) {
+                explanations = Explanation(it)
+            }
+            settingsRow("макс разрешение изображения", set.imageResolution) {
+                explanations = Explanation(it)
+            }
+        }
+        textSI(explanations.actual?:explanations.default)
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun <T>settingsRow(text: String, settings: SettingsState.SettingProperty<T>) {
+fun <T> settingsRow(text: String, settings: SettingsState.SettingProperty<T>, explainSetting: (String?) -> Unit) {
     Row(
         modifier = Modifier
             .padding(1.dp)
             .fillMaxWidth()
+            .onPointerEvent(PointerEventType.Enter) {
+                explainSetting(settings.explanations)
+            }
+            .onPointerEvent(PointerEventType.Exit) {
+                explainSetting(null)
+            }
     ) {
         textSI(text, modifier = Modifier.fillMaxWidth(0.8f))
         dropdownSettings(settings.values, settings, Modifier.fillMaxWidth())
@@ -35,7 +67,7 @@ fun <T>settingsRow(text: String, settings: SettingsState.SettingProperty<T>) {
 }
 
 @Composable
-fun <T>dropdownSettings(items: List<String>, settings: SettingsState.SettingProperty<T>, modifier: Modifier) = Box {
+fun <T> dropdownSettings(items: List<String>, settings: SettingsState.SettingProperty<T>, modifier: Modifier) = Box {
     var expanded by remember { mutableStateOf(false) }
 
     textButton(settings.currentValueString(), modifier = modifier) { expanded = true }
